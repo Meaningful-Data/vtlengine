aae801786ff8ac3ccefb41f471842a50513c0eb3
Alberto <alberto.hernandez@meaningfuldata.eu>
2024-07-29 13:37:55 +0200

    Added tests three value logic and its python file.

--- testSuite/ThreeValueLogic/test_three_value_logic.py
+++ testSuite/ThreeValueLogic/test_three_value_logic.py
@@ -7,17 +7,21 @@ Add to vs code User settings:
     }
 
 """
-from VtlEngine.Datamodel import DataSet
-from VtlEngine.Datamodel import Scalar
-from VtlEngine.DataTypes import ScalarTypes
-from VtlEngine.API.API import API
+import json
+from pathlib import Path
+from unittest import TestCase
 
-from tests import TestHelper
+import pandas as pd
 
 # Others
 import unittest
 import os
-from typing import List
+from typing import List, Dict
+
+from API import create_ast
+from DataTypes import SCALAR_TYPES
+from Interpreter import InterpreterAnalyzer
+from Model import Component, Role, Dataset
 
 """
 Improvements:
@@ -27,67 +31,94 @@ Improvements:
 TODOS:
     [1]: Add test with Datasets and Componets with null values for Numeric, Comparison and Boolean Operators.
 """
+
+
 # Path Selection.---------------------------------------------------------------
-Path = os.path.dirname(__file__)
-filepath_json = os.path.join(os.path.join(os.path.join(Path, "data"), "DataStructure"), "input")
-filepath_csv = os.path.join(os.path.join(os.path.join(Path, "data"), "DataSet"), "input")
-filepath_out_json = os.path.join(os.path.join(os.path.join(Path, "data"), "DataStructure"), "output")
-filepath_out_csv = os.path.join(os.path.join(os.path.join(Path, "data"), "DataSet"), "output")
-# File extensions.--------------------------------------------------------------
-JSON = '.json'
-CSV = '.csv'
-VTL = '.vtl'
 
-check_dtype = False  # remove when refactor complete.
-
-classTest = None
-
-
-class ThreeValueHelper(TestHelper.TestHelper):
+class ThreeValueHelper(TestCase):
     """
 
     """
-    # Path Selection.---------------------------------------------------------------
-    Path = os.path.dirname(__file__)
-    filepath_VTL = os.path.join(os.path.join(Path, "data"), "vtl")
-    filepath_json = os.path.join(os.path.join(os.path.join(Path, "data"), "DataStructure"), "input")
-    filepath_csv = os.path.join(os.path.join(os.path.join(Path, "data"), "DataSet"), "input")
-    filepath_out_json = os.path.join(os.path.join(os.path.join(Path, "data"), "DataStructure"), "output")
-    filepath_out_csv = os.path.join(os.path.join(os.path.join(Path, "data"), "DataSet"), "output")
+
+    base_path = Path(__file__).parent
+    filepath_json = base_path / "data" / "DataStructure" / "input"
+    filepath_csv = base_path / "data" / "DataSet" / "input"
+    filepath_vtl = base_path / "data" / "vtl"
+    filepath_out_json = base_path / "data" / "DataStructure" / "output"
+    filepath_out_csv = base_path / "data" / "DataSet" / "output"
+    # File extensions.--------------------------------------------------------------
+    JSON = '.json'
+    CSV = '.csv'
+    VTL = '.vtl'
 
     @classmethod
-    def LoadInputs(cls, code: str, number_inputs: int) -> List[DataSet.DataSet]:
+    def LoadDataset(cls, ds_path, dp_path):
+        with open(ds_path, 'r') as file:
+            structures = json.load(file)
+
+        for dataset_json in structures['datasets']:
+            dataset_name = dataset_json['name']
+            components = {
+                component['name']: Component(name=component['name'],
+                                             data_type=SCALAR_TYPES[component['type']],
+                                             role=Role(component['role']),
+                                             nullable=component['nullable'])
+                for component in dataset_json['DataStructure']}
+            data = pd.read_csv(dp_path, sep=',')
+
+            return Dataset(name=dataset_name, components=components, data=data)
+
+    @classmethod
+    def LoadInputs(cls, code: str, number_inputs: int) -> Dict[str, Dataset]:
         '''
 
         '''
-        input_jsons = [code + '-' + 'DS_' + str(i + 1) + cls.JSON for i in range(number_inputs)]
-        input_csvs = [code + '-' + 'DS_' + str(i + 1) + cls.CSV for i in range(number_inputs)]
+        datasets = {}
+        for i in range(number_inputs):
+            json_file_name = str(cls.filepath_json / f"{code}-DS_{str(i + 1)}{cls.JSON}")
+            csv_file_name = str(cls.filepath_csv / f"{code}-DS_{str(i + 1)}{cls.CSV}")
+            dataset = cls.LoadDataset(json_file_name, csv_file_name)
+            datasets[dataset.name] = dataset
 
-        input_datasets = []
-        for json in input_jsons:
-            input_datasets += DataSet.DataSet.load(cls.filepath_json, json, cls.filepath_csv, input_csvs)
-            if len(input_datasets) == number_inputs:
-                break
-        return input_datasets
+        return datasets
+
+    @classmethod
+    def LoadOutputs(cls, code: str, references_names: List[str]) -> Dict[str, Dataset]:
+        """
+
+        """
+        datasets = {}
+        for name in references_names:
+            json_file_name = str(cls.filepath_out_json / f"{code}-{name}{cls.JSON}")
+            csv_file_name = str(cls.filepath_out_csv / f"{code}-{name}{cls.CSV}")
+            dataset = cls.LoadDataset(json_file_name, csv_file_name)
+            datasets[dataset.name] = dataset
+
+        return datasets
+
+    @classmethod
+    def LoadVTL(cls, code: str) -> str:
+        """
+
+        """
+        vtl_file_name = str(cls.filepath_vtl / f"{code}{cls.VTL}")
+        with open(vtl_file_name, 'r') as file:
+            return file.read()
 
     @classmethod
     def BaseTest(cls, text: str, code: str, number_inputs: int, references_names: List[str]):
         '''
 
         '''
-        # Data Loading.---------------------------------------------------------
-        if text == None:
-            vtlText = cls.ScriptLoad(code=code)
-        else:
-            vtlText = text
-        inputs = cls.LoadInputs(code=code, number_inputs=number_inputs)
+        if text is None:
+            text = cls.LoadVTL(code)
+        ast = create_ast(text)
+        input_datasets = cls.LoadInputs(code, number_inputs)
+        reference_datasets = cls.LoadOutputs(code, references_names)
+        interpreter = InterpreterAnalyzer(input_datasets)
+        result = interpreter.visit(ast)
+        assert result == reference_datasets
 
-        # Interpreter Execution.------------------------------------------------
-        results = API.VtlRunOnly(text=vtlText, dataSets=inputs, return_only_persistent=False)
-
-        # Test Assertion.-------------------------------------------------------
-        references = cls.LoadOutputs(code=code, references_names=references_names)
-        cls.AssertDataSets(references, results)
 
 class ThreeValueTests(ThreeValueHelper):
     """
@@ -107,7 +138,6 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
 
     def test_2(self):
@@ -119,7 +149,6 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
 
     def test_3(self):
@@ -131,7 +160,6 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
 
     def test_4(self):
@@ -143,7 +171,6 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
 
     def test_5(self):
@@ -155,7 +182,6 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
 
     def test_6(self):
@@ -167,7 +193,6 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
 
     def test_7(self):
@@ -179,5 +204,4 @@ class ThreeValueTests(ThreeValueHelper):
         number_inputs = 1
         references_names = ["DS_r"]
 
-        self.SemanticTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
         self.BaseTest(text=text, code=code, number_inputs=number_inputs, references_names=references_names)
