e5e2dc598d81d0bd2768a75f17e8509e433c8de2
Alberto <alberto.hernandez@meaningfuldata.eu>
2024-08-23 14:35:58 +0200

    External proyects tests added.

--- testSuite/BigProyects/ExternalProyects/test_ext_proyects.py
+++ testSuite/BigProyects/ExternalProyects/test_ext_proyects.py
@@ -88,7 +88,8 @@ class ExternalProjectsHelper(TestCase):
             return file.read()
 
     @classmethod
-    def BaseTest(cls, code: str, number_inputs: int, references_names: List[str], vd_names: List[str] = None, sql_names:List[str]=None):
+    def BaseTest(cls, code: str, number_inputs: int, references_names: List[str], vd_names: List[str] = None,
+                 sql_names: List[str] = None):
         '''
 
         '''
@@ -131,3 +132,155 @@ class ExternalProjectsHelper(TestCase):
             with open(sql_file_name, 'r') as file:
                 external_routines[name] = ExternalRoutine.from_sql_query(name, file.read())
         return external_routines
+
+
+class BOP(ExternalProjectsHelper):
+    """
+
+    """
+
+    classTest = 'ExternalProjects.BOP'
+
+    def test_BOP_Q_Review_1(self):
+        """
+        Description:
+        Git Branch: feat-test-projects
+        Goal: Check semantic result and interpreter results.
+        """
+        code = 'BOP_Q_Review_1'
+        number_inputs = 1
+        rn = [str(i) for i in range(1, 30)]
+        references_names = rn
+
+        self.BaseTest(code=code, number_inputs=number_inputs, references_names=references_names)
+
+
+class AnaVal(ExternalProjectsHelper):
+    """
+
+    """
+
+    classTest = 'ExternalProjects.AnaVal'
+
+    def test_Monthly_validations_1(self):
+        """
+        Description: AT_Bank_201906
+        Git Branch: feat-test-projects
+        Goal: Check semantic result and interpreter results.
+        """
+        code = 'AnaVal_Monthly_validations_1'
+        number_inputs = 36
+        vd_names = ["EU_countries", "AnaCreditCountries_1"]
+        rn = [str(i) for i in range(1, 288)]
+        references_names = rn
+
+        self.BaseTest(
+            code=code,
+            number_inputs=number_inputs,
+            references_names=references_names,
+            vd_names=vd_names
+        )
+
+    def test_Quarterly_validations_1(self):
+        """
+        Description: AT_Bank_201906
+        Git Branch: feat-test-projects
+        Goal: Check semantic result and interpreter results.
+        """
+        code = 'AnaVal_Quarterly_validations_1'
+        number_inputs = 12
+        vd_names = ["EU_countries", "AnaCreditCountries_1"]
+        rn = [str(i) for i in range(1, 38)]
+        references_names = rn
+
+        self.BaseTest(
+            code=code,
+            number_inputs=number_inputs,
+            references_names=references_names,
+            vd_names=vd_names
+        )
+
+    def test_GL_283_1(self):
+        """
+        USER DEFINED OPERATORS
+        Status: OK
+        Description:
+        Git Branch: #283
+        """
+        code = 'GL_283_1'
+        number_inputs = 36
+        vd_names = ["EU_countries", "AnaCreditCountries_1"]
+        rn = [str(i) for i in range(1, 129)]
+        references_names = rn
+
+        self.BaseTest(code=code, number_inputs=number_inputs, references_names=references_names, vd_names=vd_names)
+
+
+class AnaMart(ExternalProjectsHelper):
+    """
+
+    """
+
+    classTest = 'ExternalProjects.AnaMart'
+
+    def test_AnaMart_AnaMart_1(self):
+        """
+        Description: AT_Bank_201906
+        Git Branch: feat-test-projects
+        Goal: Check semantic result and interpreter results.
+        """
+        code = 'AnaMart_AnaMart_1'
+        number_inputs = 30
+        vd_names = ["anaCreditCountries_2"]
+        # rn = [str(i) for i in range(1, 303)]
+        rn = [str(i) for i in range(1, 30)]
+        rn += [str(i) for i in range(72, 303)]
+        references_names = rn
+        sql_names = [
+            "instDates",
+            "instrFctJn",
+            "instrFctJn2",
+            "prtctnDts",
+            "prtctnFctJn"
+        ]
+
+        self.BaseTest(
+            code=code,
+            number_inputs=number_inputs,
+            references_names=references_names,
+            vd_names=vd_names,
+            sql_names=sql_names
+        )
+        exception_code = "1-1-13-4"
+        self.NewSemanticExceptionTest(
+            code=code, number_inputs=number_inputs, vd_names=vd_names, exception_code=exception_code
+        )
+
+    def test_AnaMart_AnaMart_2(self):
+        """
+        Description: AT_Bank_201906
+        Git Branch: feat-test-projects
+        Goal: Check semantic result and interpreter results.
+        """
+        code = 'AnaMart_AnaMart_2'
+        number_inputs = 30
+        vd_names = ["anaCreditCountries_2"]
+        # rn = [str(i) for i in range(1, 303)]
+        rn = [str(i) for i in range(1, 30)]
+        rn += [str(i) for i in range(72, 303)]
+        references_names = rn
+        sql_names = [
+            "instDates",
+            "instrFctJn",
+            "instrFctJn2",
+            "prtctnDts",
+            "prtctnFctJn"
+        ]
+
+        self.BaseTest(
+            code=code,
+            number_inputs=number_inputs,
+            references_names=references_names,
+            vd_names=vd_names,
+            sql_names=sql_names
+        )
