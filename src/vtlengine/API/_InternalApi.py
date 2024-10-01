612203caccac0cab1a5ceb9c3dfbd964593454b8
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2024-10-01 15:50:43 +0200

    Fixed load vtl to create path from string.

--- src/vtlengine/API/_InternalApi.py
+++ src/vtlengine/API/_InternalApi.py
@@ -197,7 +197,11 @@ def load_vtl(input: Union[str, Path]):
     the file.
     """
     if isinstance(input, str):
-        return input
+        try:
+            test = Path(input)
+        except Exception:
+            return input
+        input = test
     if not isinstance(input, Path):
         raise Exception('Invalid vtl file. Input is not a Path object')
     if not input.exists():
