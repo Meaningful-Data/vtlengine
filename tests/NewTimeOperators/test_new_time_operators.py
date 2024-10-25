b1dadf8271759077908217bfa92e883dc5ce200f
Alberto <alberto.hernandez@meaningfuldata.eu>
2024-10-25 14:16:45 +0200

    Tests with vtl expressions for year, month, day of year and day of month operators were added. Deleted scalar and dc tests.

--- tests/NewTimeOperators/test_new_time_operators.py
+++ tests/NewTimeOperators/test_new_time_operators.py
@@ -1,46 +1,51 @@
 import pandas as pd
 import pytest
+from pandas.testing import assert_series_equal
 
 from vtlengine import DataTypes
-from vtlengine.DataTypes import TimePeriod
+from vtlengine.API import create_ast
+from vtlengine.DataTypes import Date, Integer, String, TimeInterval
 from vtlengine.Exceptions import SemanticError
-from vtlengine.Model import Scalar, Role, DataComponent, Dataset, Component
-from vtlengine.Operators.Time import Month, Year, Day_of_Month, Day_of_Year
+from vtlengine.Interpreter import InterpreterAnalyzer
+from vtlengine.Model import Scalar, Role, Dataset, Component
+from vtlengine.Operators.Time import Month
 
-scalar_params = [
-    (Month, "2022-01-23", 1),
-    (Month, "2022Q1", 1),
-    (Month, "2022Q4", 10),
-    (Month, "2022-09-24", 9),
-    (Year, "2022Q3", 2022),
-    (Year, "2022-01-23", 2022),
-    (Day_of_Month, "2022-01-23", 23),
-    (Day_of_Month, "2022Q1", 31),
-    (Day_of_Year, "2022-02-15", 46),
-    (Day_of_Year, "2024Q1", 91),
-]
-
-dc_params = [
-    (Month, pd.Series(name="TEST", data=["2022Q1", "2023-05-26"]), [1, 5]),
-    (Month, pd.Series(name="TEST", data=["2022Q4", "2023-05-26"]), [10, 5]),
-    (Year, pd.Series(name="TEST", data=["2022Q1", "2023-05-26"]), [2022, 2023]),
-    (Day_of_Month, pd.Series(name="TEST", data=["2024Q2", "2023-01-12"]), [30, 12]),
-    (Day_of_Year, pd.Series(name="TEST", data=["2024Q1", "2023-01-12"]), [91, 12]),
-]
-
-error_params_scalar = [
-    (Month, "2022 / 01", "2-1-19-11"),
-    (Year, "2022 / 01", "2-1-19-11"),
-    (Day_of_Month, "2022 / 03", "2-1-19-11"),
-    (Day_of_Year, "2022 / 03", "2-1-19-11"),
-]
-
-error_params_dc = [
-    (Month, pd.Series(name="TEST", data=["2022 / 01", "2023-05-26"]), "2-1-19-11"),
-    (Year, pd.Series(name="TEST", data=["2022 / 01", "2023-05-26"]), "2-1-19-11"),
-    (Day_of_Month, pd.Series(name="TEST", data=["2024 / 02", "2023-01-12"]), "2-1-19-11"),
-    (Day_of_Year, pd.Series(name="TEST", data=["2024 / 02", "2023-01-12"]), "2-1-19-11"),
-]
+ds_1 = Dataset(
+    name="DS_1",
+    components={
+        "Id_1": Component(name="Id_1", data_type=String, role=Role.IDENTIFIER, nullable=False),
+        "Me_1": Component(name="Me_1", data_type=Date, role=Role.MEASURE, nullable=True),
+    },
+    data=pd.DataFrame(
+        columns=["Id_1", "Me_1"],
+        index=[0, 1, 2],
+        data=[("A", "2022-01-30"), ("B", "2022-08-21"), ("C", None)],
+    ),
+)
+ds_2 = Dataset(
+    name="DS_1",
+    components={
+        "Id_1": Component(name="Id_1", data_type=String, role=Role.IDENTIFIER, nullable=False),
+        "Me_1": Component(name="Me_1", data_type=String, role=Role.MEASURE, nullable=True),
+    },
+    data=pd.DataFrame(
+        columns=["Id_1", "Me_1"],
+        index=[0, 1, 2],
+        data=[("A", "2022-01-30"), ("B", "2022-08-21"), ("C", None)],
+    ),
+)
+ds_3 = Dataset(
+    name="DS_1",
+    components={
+        "Id_1": Component(name="Id_1", data_type=String, role=Role.IDENTIFIER, nullable=False),
+        "Me_1": Component(name="Me_1", data_type=TimeInterval, role=Role.MEASURE, nullable=True),
+    },
+    data=pd.DataFrame(
+        columns=["Id_1", "Me_1"],
+        index=[0, 1, 2],
+        data=[("A", "2022-01-30"), ("B", "2022-08-21"), ("C", None)],
+    ),
+)
 
 ds_error_params = [
     (
@@ -54,33 +59,23 @@ ds_error_params = [
     )
 ]
 
+vtl_expression_test_params = [
+    ('year(cast("2023-01-12", date))', 2023),
+    ('year(cast("2022Q1", time_period))', 2022),
+    ("DS_1[calc Me_2 := year(Me_1)]", pd.Series(name="Me_2", data=[2022, 2022, None])),
+    ('month(cast("2023-01-12", date))', 1),
+    ('month(cast("2022Q1", time_period))', 1),
+    ("DS_1[calc Me_2 := month(Me_1)]", pd.Series(name="Me_2", data=[1, 8, None])),
+    ('dayofmonth(cast("2023-01-12", date))', 12),
+    ('dayofmonth(cast("2022Q1", time_period))', 31),
+    ("DS_1[calc Me_2 := dayofmonth(Me_1)]", pd.Series(name="Me_2", data=[30, 21, None])),
+    ('dayofyear(cast("2023-01-12", date))', 12),
+    ('dayofyear(cast("2022Q1", time_period))', 90),
+    ("DS_1[calc Me_2 := dayofyear(Me_1)]", pd.Series(name="Me_2", data=[30, 233, None])),
+]
+me_str_params = [("DS_1[calc Me_2 := year(Me_1)]", "1-1-1-1")]
 
-@pytest.mark.parametrize("op, value, reference", scalar_params)
-def test_unary_time_operator_scalar(op, value, reference):
-    scalar = Scalar(value, TimePeriod, value)
-    result = op.evaluate(scalar)
-    assert reference == result.value
-
-
-@pytest.mark.parametrize("op, value, reference", dc_params)
-def test_unary_time_operator_dc(op, value, reference):
-    dc = DataComponent("TEST", value, TimePeriod, Role.MEASURE, False)
-    result = op.evaluate(dc)
-    assert all(reference == result.data.values)
-
-
-@pytest.mark.parametrize("op, value, code", error_params_scalar)
-def test_error_scalar(op, value, code):
-    scalar = Scalar(value, TimePeriod, value)
-    with pytest.raises(SemanticError, match="2-1-19-11"):
-        op.evaluate(scalar)
-
-
-@pytest.mark.parametrize("op, value, code", error_params_dc)
-def test_error_dc(op, value, code):
-    dc = DataComponent("TEST", value, TimePeriod, Role.MEASURE, False)
-    with pytest.raises(SemanticError, match="2-1-19-11"):
-        op.evaluate(dc)
+me_time_interval_params = [("DS_1[calc Me_2 := year(Me_1)]", "1-1-1-1")]
 
 
 @pytest.mark.parametrize("op, value, code", ds_error_params)
@@ -99,3 +94,30 @@ def test_ds_error(op, value, code):
     )
     with pytest.raises(SemanticError, match="1-1-19-8"):
         op.evaluate(ds)
+
+
+@pytest.mark.parametrize("text, reference", vtl_expression_test_params)
+def test_vtl_expression_unary_time_op(text, reference):
+    expression = f"DS_r := {text};"
+    ast = create_ast(expression)
+    interpreter = InterpreterAnalyzer({"DS_1": ds_1})
+    result = interpreter.visit(ast)
+    if isinstance(result["DS_r"], Scalar):
+        assert result["DS_r"].value == reference
+        assert result["DS_r"].data_type == Integer
+    else:
+        assert_series_equal(result["DS_r"].data["Me_2"], reference)
+
+
+@pytest.mark.parametrize("text, code", me_str_params)
+def test_vtl_expression_unary_time_op_me_str(text, code):
+    expression = f"DS_r := {text};"
+    ast = create_ast(expression)
+    interpreter = InterpreterAnalyzer({"DS_1": ds_2})
+    with pytest.raises(SemanticError, match="1-1-1-1"):
+        interpreter.visit(ast)
+
+
+@pytest.mark.parametrize("text, reference", me_time_interval_params)
+def test_vtl_expression_unary_time_op_me_time_interval(text, reference):
+    pass
