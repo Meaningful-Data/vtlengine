5ee061ed0a458c424a17bd18898238d25d26fbc1
Mateo de Lorenzo Argelés <160473799+mla2001@users.noreply.github.com>
2024-11-14 13:15:02 +0100

    15 add implementation of dateadd operator (#44)
    * Fixed DateAdd token. Added interpreter method ofr DateAdd. DateAdd working with Date inputs.
    
    * Fixed mypy and ruff errors.
    
    * DateAdd operator now fully works with TimePeriod. Refactored to always return Date values.
    
    * Added DateAdd test suite.
    
    * Removed TimePeriod 'YYYY' and 'YYYY-MM' format handler. Removed unused error message. Solved mypy and ruff errors.
    
    * Changed op_date to py_op.
    
    * Fixed ruff error.

---
+++ tests/NewOperators/Time/conftest.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+import pandas as pd
+import pytest
+
+from vtlengine.API._InternalApi import load_datasets_with_data
+
+
+def load_datasets(base_path, code, folder_type):
+    datapoints_path = base_path / 'DataSet' / folder_type
+    input_path = base_path / 'DataStructure' / folder_type
+
+    num_inputs = len([f for f in os.listdir(input_path) if f.startswith(f"{code}-")])
+    datasets = {}
+
+    for i in range(1, num_inputs + 1):
+        with open(input_path / f"{code}-{i}.json", 'r') as file:
+            datastructure = json.load(file)
+
+        ds_name = datastructure['datasets'][0]['name']
+        datapoint = {ds_name: pd.read_csv(datapoints_path / f"{code}-{i}.csv")}
+        datasets.update(load_datasets_with_data(datastructure, datapoint)[0])
+
+    return datasets
+
+
+@pytest.fixture
+def load_input(request, code):
+    base_path = request.node.get_closest_marker('input_path').args[0]
+    return load_datasets(base_path, code, folder_type="input")
+
+
+@pytest.fixture
+def load_reference(request, code):
+    base_path = request.node.get_closest_marker('input_path').args[0]
+    return load_datasets(base_path, code, folder_type="output")
