c78385cb182e152df0e96acd93b388b60d361721
Alejandra Caballero Pertíñez <valeriedin35@gmail.com>
2024-11-15 15:43:35 +0100

    Test of the datediff operator that still does not work correctly

---
+++ tests/NewOperators/Time/test_datediff.py
@@ -0,0 +1,41 @@
+import pytest
+import warnings
+
+from pytest import mark
+from pathlib import Path
+
+from vtlengine.API import create_ast
+from vtlengine.Exceptions import SemanticError
+from vtlengine.Interpreter import InterpreterAnalyzer
+
+pytestmark = mark.input_path(Path(__file__).parent / "data")
+
+ds_param = [
+    ("21", 'DS_r := DS_1[calc Me_3 := datediff(Me_1, Me_2)];'),
+]
+
+error_param = [
+    ("22", 'DS_r := DS_1[calc Me_3 := datediff(Me_1, Me_2)];', "1-1-1-1"),
+]
+
+@pytest.mark.parametrize("code, expression", ds_param)
+def test_case_ds(load_input, load_reference, code, expression):
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    ast = create_ast(expression)
+    interpreter = InterpreterAnalyzer(load_input)
+    result = interpreter.visit(ast)
+    assert result == load_reference
+
+
+@pytest.mark.parametrize("code, expression, error_code", error_param)
+def test_errors(load_input, code, expression, error_code):
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    datasets = load_input
+    with pytest.raises(SemanticError) as context:
+        ast = create_ast(expression)
+        interpreter = InterpreterAnalyzer(datasets)
+        interpreter.visit(ast)
+    result = error_code == str(context.value.args[1])
+    if result is False:
+        print(f"\n{error_code} != {context.value.args[1]}")
+    assert result
