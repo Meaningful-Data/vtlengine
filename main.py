b0f26500d0a3cd918e0d69cae7ff591de7964f7e
Alejandra Caballero Pertíñez <valeriedin35@gmail.com>
2024-11-19 15:01:05 +0100

    Main Changed

--- main.py
+++ main.py
@@ -4,25 +4,9 @@ from vtlengine import run
 
 
 def main():
-    #127
-    #script = """
-    #    DS_A := datediff("2020-12-14","2021-04-20");
-    #"""
-
-    #456
-    #script = """
-    #    DS_A := datediff("2022Q1", "2023Q2");
-    #"""
-
-    #127
     script = """
-        DS_A := datediff("2021Q2","2021-11-04");
+        DS_A := DS_1 * 10;
     """
-
-    # 4,5,4
-    #script = """
-    #     DS_A := DS_1[calc Me_3 := datediff(Me_1, Me_2)];
-    #"""
 
     data_structures = {
         'datasets': [
@@ -34,11 +18,7 @@ def main():
                   'role': 'Identifier',
                   'nullable': False},
                  {'name': 'Me_1',
-                  'type': 'Date',
-                  'role': 'Measure',
-                  'nullable': True},
-                 {'name': 'Me_2',
-                  'type': 'Date',
+                  'type': 'Number',
                   'role': 'Measure',
                   'nullable': True}
              ]
@@ -48,15 +28,14 @@ def main():
 
     data_df = pd.DataFrame(
         {"Id_1": [1, 2, 3],
-         "Me_1": ["2020-01-05", "2020-02-06", "2020-03-05"],
-         "Me_2":  ["2020-01-01", "2020-02-01", "2020-03-01"]})
+         "Me_1": [10, 20, 30]})
 
     datapoints = {"DS_1": data_df}
 
     run_result = run(script=script, data_structures=data_structures,
                      datapoints=datapoints)
 
-    print(run_result['DS_A'])
+    print(run_result)
 
 
 if __name__ == '__main__':
