b8f93f8cce306d110ee2e00c1c66f60966215e2a
Alberto <155883871+albertohernandez1995@users.noreply.github.com>
2024-12-31 12:31:21 +0100

    Amend AST import in ASTEncoders module. Encode and Decode AST tests were created.

--- tests/AST/test_AST.py
+++ tests/AST/test_AST.py
@@ -1,17 +1,35 @@
 import json
+from pathlib import Path
 
+import pytest
 from vtlengine.API import load_vtl, create_ast
 from vtlengine.AST.ASTEncoders import ComplexEncoder, ComplexDecoder
 
-# AST generation
-vtl = load_vtl(script)
-ast = create_ast(vtl)
-print(ast)
 
-# Encode to JSON
-result = json.dumps(ast, indent=4, cls=ComplexEncoder)
-print(result)
+base_path = Path(__file__).parent
+filepath = base_path / "data" / "encode"
 
-# Decode from JSON
-ast = json.loads(result, object_hook=ComplexDecoder.object_hook)
-print(ast)
+param = [
+    "DS_r := DS_1 + DS_2;"
+]
+
+
+@pytest.mark.parametrize("script", param)
+def test_encode_ast(script):
+    vtl = load_vtl(script)
+    ast = create_ast(vtl)
+    result = json.dumps(ast, indent=4, cls=ComplexEncoder)
+    with open(filepath / "reference_encode.json", 'r') as file_reference:
+        reference = file_reference.read()
+    assert result == reference
+
+
+@pytest.mark.parametrize("script", param)
+def test_decode_ast(script):
+    vtl = load_vtl(script)
+    ast = create_ast(vtl)
+    with open(filepath / 'reference_encode.json') as file:
+        ast_decode = json.load(file, object_hook=ComplexDecoder.object_hook)
+    assert ast_decode == ast
+
+
