b7de48db8624db13eef892b186250638d397c763
Mateo de Lorenzo Argelés <160473799+mla2001@users.noreply.github.com>
2025-02-21 14:59:38 +0100

    74 generation of a complete vtl test (#78)
    * Added whole VTL grammar basic test.
    
    * Fixed mypy and ruff errors.
    
    * Fixed ubuntu path not found.
    
    * Added Time tests.
    
    * Added Set tests.
    
    * Added missing Comparison tests.
    
    * Added ValueDomains tests. Updated tests references.
    
    * Updated tests references.
    
    * Fixed ruff errors.
    
    * Added missing tests.
    
    * Fixed columns are compared in wrong order.
    
    ---------
    
    Co-authored-by: Francisco Javier Hernández del Caño <javier.hernandez@meaningfuldata.eu>

--- tests/Complete_VTL_Grammar/test_grammar.py
+++ tests/Complete_VTL_Grammar/test_grammar.py
@@ -13,9 +13,11 @@ dataset_output_path = base_path / "DataSet" / "output"
 datastructure_input_path = base_path / "DataStructure" / "input"
 datastructure_output_path = base_path / "DataStructure" / "output"
 sql_path = base_path / "SQL"
+vd_path = base_path / "ValueDomain"
 vtl_path = base_path / "vtl"
 
 def test_grammar():
+    refactor_results = False
     script_name = "test_grammar.vtl"
     with open(vtl_path / script_name, "r") as file:
         script = file.read()
@@ -23,13 +25,54 @@ def test_grammar():
     sql_name = "SQL1.sql"
     external_routines = sql_path / sql_name
 
+    vd_name = "countries"
+    value_domains = vd_path / f"{vd_name}.json"
+
     data_structures, datapoints = load_input_data("DS_1")
+
+    run_result = run(script=script,
+                     data_structures=data_structures,
+                     datapoints=datapoints,
+                     external_routines=external_routines,
+                     value_domains=value_domains)
+
+    if refactor_results:
+        store_results(run_result)
     reference_datasets, reference_scalars = load_reference_data()
+    check_results(run_result, reference_datasets, reference_scalars)
 
-    run_result = run(script=script, data_structures=data_structures,
-                     datapoints=datapoints, external_routines=external_routines)
 
+def store_results(run_result):
+    reference_json = {'datasets': [], 'scalars': []}
     for result in run_result.values():
+        if isinstance(result, Dataset):
+            result.data.to_csv(dataset_output_path / f"{result.name}.csv", index=False)
+            structure = result.to_dict()
+            components = structure.pop('components')
+            structured_components = [{'name': c['name'],
+                                      'type': c['data_type'],
+                                      'role': c['role'],
+                                      'nullable': c['nullable'],
+                                      } for c in components.values()]
+            structure['DataStructure'] = structured_components
+            structure.pop('data')
+            reference_json['datasets'].append(structure)
+        else:
+            scalar = {
+                'name': result.name,
+                'data_type': result.data_type.__name__,
+                'value': result.value
+            }
+            reference_json['scalars'].append(scalar)
+
+    with open(datastructure_output_path / "reference.json", "w") as file:
+        json.dump(reference_json, file)
+
+
+def check_results(run_result, reference_datasets, reference_scalars):
+    for result in run_result.values():
+        if result.name == "current_date_sc":
+            continue
         if isinstance(result, Dataset):
             assert result.name in reference_datasets
             reference = reference_datasets[result.name]
@@ -37,8 +80,9 @@ def test_grammar():
             assert len(result.components) == len(reference.components)
             assert result.components == reference.components
 
-            dataset_data = result.data.reset_index(drop=True)
-            reference_data = reference.data.reset_index(drop=True)
+            sorted_columns = sorted(result.data.columns)
+            dataset_data = result.data[sorted_columns].reset_index(drop=True)
+            reference_data = reference.data[sorted_columns].reset_index(drop=True)
             assert all(dataset_data == reference_data)
 
         else:
