9d27e7fef0e7f205fee4283155b2fcd0d03fde05
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-03-31 13:05:48 +0200

    Added missing properties (most of them call super(), but for some reason they were not detected from parent class, so I redefined them like this.

--- src/vtlengine/DataFrame/__init__.py
+++ src/vtlengine/DataFrame/__init__.py
@@ -5,6 +5,7 @@ from typing import IO, Any, Dict, Union
 import numpy as np
 import pandas as pd
 import polars as pl
+from polars._utils.unstable import unstable
 from polars.dependencies import polars_cloud
 from polars.interchange.utils import polars_dtype_to_dtype
 from polars.polars import PyDataFrame
@@ -41,23 +42,9 @@ elif backend_df == "pl":
 
 
     class PolarsDataFrame(pl.DataFrame):
-        """Override of polars.DataFrame with pandas-like methods"""
-        _df: PyDataFrame
-        _series: Dict[str, Union["PolarsSeries", pl.Series]] = {}
-        dtypes = {}
-        plot = None
-        schema = None
-        style = None
-
-        height = 0
-        width = 0
-        size = 0
-        shape = (0, 0)
-
         def __init__(self, data=None, columns=None):
             super().__init__(data)
-            self._series = {}  # Dictionary of columns: name -> PolarsSeries
-
+            self._series = {}
             if data is None:
                 if columns is not None:
                     for col in columns:
@@ -78,20 +65,12 @@ elif backend_df == "pl":
                     self._series[col] = PolarsSeries(data[col].to_list(), name=col)
             else:
                 raise ValueError("Unsupported data type for creating PolarsDataFrame.")
-
             self._build_df()
 
         def _build_df(self):
             d = {col: series.to_list() for col, series in self._series.items()}
             self._df = pl.DataFrame(d)
-
-            self.height = len(self._df)
-            self.width = len(self._df.columns)
-            self.shape = self._df.shape
-            self.size = self._df.shape[0] * self._df.shape[1]
-
-            self.dtypes = {col: series.dtype for col, series in self._series.items()}
-            self.plot = self
+            # self.dtypes = {col: series.dtype for col, series in self._series.items()}
 
         def __getitem__(self, key):
             if isinstance(key, str):
@@ -114,6 +93,9 @@ elif backend_df == "pl":
         def _repr_html_(self, *args, **kwargs):
             return super()._repr_html_(*args, **kwargs)
 
+        def __str__(self):
+            return super().__str__()
+
         class _ColumnsWrapper:
             def __init__(self, columns):
                 self._columns = columns
@@ -133,9 +115,49 @@ elif backend_df == "pl":
             def __repr__(self):
                 return repr(self._columns)
 
+            def __str__(self):
+                return str(self._columns)
+
         @property
         def columns(self):
             return self._ColumnsWrapper(self._df.columns)
+
+        @property
+        def dtypes(self):
+            return self._df.dtypes
+
+        # @dtypes.setter
+        # def dtypes(self, value):
+        #     self._df.dtypes = value
+
+        @property
+        def empty(self):
+            return self.__len__() == 0
+
+        @property
+        def height(self) -> int:
+            return self._df.height
+
+        @property
+        @unstable()
+        def plot(self):
+            return super().plot
+
+        @property
+        def shape(self) -> tuple[int, int]:
+            return super().shape
+
+        @property
+        def size(self) -> int:
+            return self.height * self.width
+
+        @property
+        def values(self):
+            return self.to_list()
+
+        @property
+        def width(self) -> int:
+            return super().width
 
         def copy(self):
             return self.clone()
@@ -161,14 +183,23 @@ elif backend_df == "pl":
                         new_data = [value[col] if x is None else x for x in series.to_list()]
                     else:
                         new_data = series.to_list()
-                    new_data = [None if x != x else x for x in new_data]  # Convert NaN to None
+                    new_data = [None if x != x else x for x in new_data]
                     new_series[col] = PolarsSeries(new_data, name=col)
             else:
                 for col, series in self._series.items():
                     new_data = [value if x is None else x for x in series.to_list()]
-                    new_data = [None if x != x else x for x in new_data]  # Convert NaN to None
+                    new_data = [None if x != x else x for x in new_data]
                     new_series[col] = PolarsSeries(new_data, name=col)
             return PolarsDataFrame(new_series)
+
+        def rename(self, columns: dict, inplace: bool = False):
+            new_series = {columns.get(col, col): series for col, series in self._series.items()}
+            if inplace:
+                self._series = new_series
+                self._build_df()
+                return None
+            else:
+                return PolarsDataFrame(new_series)
 
         def replace(self, to_replace, value=None, **kwargs):
             if isinstance(to_replace, dict):
@@ -176,7 +207,6 @@ elif backend_df == "pl":
                 for old, new in to_replace.items():
                     df_temp = df_temp.replace(old, new, **kwargs)
                 return df_temp
-
             new_data = {}
             for col in self.columns.tolist():
                 series = self._series[col].to_list()
@@ -186,15 +216,16 @@ elif backend_df == "pl":
                     for x in series
                 ]
                 new_data[col] = new_series
-
             return PolarsDataFrame(new_data)
 
+        def sort_values(self, by: str, ascending: bool = True):
+            sorted_df = self._df.sort(by, reverse=not ascending)
+            return PolarsDataFrame(sorted_df)
+
         def view(self):
-            """Display the DataFrame in a tabular format for debugging."""
             print(self._df)
 
         def view_series(self, column_name: str):
-            """Display a specific series (column) in a tabular format for debugging."""
             if column_name in self._series:
                 print(self._series[column_name])
             else:
@@ -226,6 +257,9 @@ elif backend_df == "pl":
 
         def copy(self):
             return self.clone()
+
+        def dropna(self):
+            return PolarsSeries(self.drop_nulls(), name=self.name)
 
         def isnull(self):
             return self.is_null()
