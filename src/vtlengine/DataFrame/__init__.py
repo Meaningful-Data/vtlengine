b825ea4585d27dd998a01da5c978089036e243fb
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-03-31 10:32:01 +0200

    Fixed fillna method avoiding usage of with_columns and pyExpr assigment before column casting.

--- src/vtlengine/DataFrame/__init__.py
+++ src/vtlengine/DataFrame/__init__.py
@@ -1,10 +1,8 @@
-import csv
 import os
 
 import numpy as np
 import pandas as pd
 import polars as pl
-import polars.functions as plr
 
 POLARS_STR = ["polars", "pl"]
 
@@ -22,33 +20,14 @@ if backend_df == "pd":
 
 elif backend_df == "pl":
 
-    class PolarsSeries(pl.Series):
-        def __init__(self, data, name=None, *args, **kwargs):
-            # We use the *args, **kwargs construction to ignore arguments incompatible with the Pandas signature
-            super().__init__(name=name, values=data)
-
-        def __repr__(self):
-            return repr(self)
-
-        def isnull(self):
-            return self.is_null()
-
-
     class PolarsDataFrame(pl.DataFrame):
         """Override of polars.DataFrame with pandas-like methods"""
 
         def __init__(self, data=None, columns=None):
-            """
-            Allows creating the DataFrame from:
-             - A dictionary: {column: data}
-             - A list: [data_col1, data_col2, ...] (columns must be specified)
-             - Instances of pl.DataFrame are also accepted (data is extracted)
-            """
             super().__init__(data)
             self._series = {}  # Dictionary of columns: name -> PolarsSeries
 
             if data is None:
-                # Empty DataFrame
                 if columns is not None:
                     for col in columns:
                         self._series[col] = PolarsSeries([], name=col)
@@ -60,12 +39,10 @@ elif backend_df == "pl":
                         self._series[col] = values
             elif isinstance(data, list):
                 if columns is None:
-                    # If columns are not specified, generate default names
                     columns = [f"col{i}" for i in range(len(data))]
                 for col_name, col_data in zip(columns, data):
                     self._series[col_name] = PolarsSeries(col_data, name=col_name)
             elif isinstance(data, pl.DataFrame):
-                # Extract data from each column
                 for col in data.columns:
                     self._series[col] = PolarsSeries(data[col].to_list(), name=col)
             else:
@@ -74,29 +51,19 @@ elif backend_df == "pl":
             self._build_df()
 
         def _build_df(self):
-            """
-            Rebuilds the internal polars DataFrame from the stored series.
-            """
             d = {col: series.to_list() for col, series in self._series.items()}
             self._df = pl.DataFrame(d)
 
         def __getitem__(self, key):
-            """
-            Accessing a column always returns the associated PolarsSeries.
-            """
             if isinstance(key, str):
                 return self._series[key]
             elif isinstance(key, list):
-                # If multiple columns are requested, create a new PolarsDataFrame
                 new_data = {col: self._series[col].to_list() for col in key if col in self._series}
                 return PolarsDataFrame(new_data)
             else:
                 raise KeyError("Unsupported index type.")
 
         def __setitem__(self, key, value):
-            """
-            When assigning a column, it is forced to be PolarsSeries.
-            """
             if not isinstance(value, PolarsSeries):
                 value = PolarsSeries(value, name=key)
             self._series[key] = value
@@ -106,8 +73,6 @@ elif backend_df == "pl":
             return repr(self)
 
         class _ColumnsWrapper:
-            """Internal class to override `columns` and add `.tolist()` method."""
-
             def __init__(self, columns):
                 self._columns = columns
 
@@ -131,67 +96,77 @@ elif backend_df == "pl":
             return self._ColumnsWrapper(self._df.columns)
 
         def fillna(self, value, *args, **kwargs):
-            """
-            Replaces missing values (None) in the DataFrame.
-
-            If 'value' is a scalar, replaces all None with that value.
-            If it is a dictionary, replaces each column with the specified value.
-            Returns a new PolarsDataFrame with the changes.
-            """
             new_series = {}
             if isinstance(value, dict):
                 for col, series in self._series.items():
                     if col in value:
-                        # Replace None with the specified value for that column
-                        new_data = [value[col] if x is None else x for x in series.data]
+                        new_data = [value[col] if x is None else x for x in series.to_list()]
                     else:
-                        new_data = series.data
+                        new_data = series.to_list()
+                    new_data = [None if x != x else x for x in new_data]  # Convert NaN to None
                     new_series[col] = PolarsSeries(new_data, name=col)
             else:
                 for col, series in self._series.items():
-                    new_data = [value if x is None else x for x in series.data]
+                    new_data = [value if x is None else x for x in series.to_list()]
+                    new_data = [None if x != x else x for x in new_data]  # Convert NaN to None
                     new_series[col] = PolarsSeries(new_data, name=col)
             return PolarsDataFrame(new_series)
 
-
         def replace(self, to_replace, value=None, **kwargs):
-            """
-            Replaces values in the DataFrame similar to pandas.DataFrame.replace.
-
-            Supports:
-             - Scalar value replacement globally.
-             - Replacement using a dictionary: {value_to_replace: new_value, ...}.
-
-            Note: This implementation is basic and can be extended to cover more complex cases.
-            """
-            # If to_replace is a dictionary, apply replacements sequentially.
             if isinstance(to_replace, dict):
                 df_temp = self
                 for old, new in to_replace.items():
                     df_temp = df_temp.replace(old, new, **kwargs)
                 return df_temp
 
-            # In the simplest case: to_replace is a scalar value.
-            new_cols = []
+            new_data = {}
             for col in self.columns.tolist():
-                new_col = pl.when(pl.col(col) == to_replace).then(value).otherwise(pl.col(col)).alias(col)
-                new_cols.append(new_col)
-            return self.with_columns(new_cols)
+                series = self._series[col].to_list()
+                new_series = [
+                    value if (x == to_replace or (to_replace is np.nan and (x != x)) or (
+                                to_replace is None and x is None)) else x
+                    for x in series
+                ]
+                new_data[col] = new_series
+
+            return PolarsDataFrame(new_data)
 
 
+    class PolarsSeries(pl.Series):
+        def __init__(self, data, name=None, *args, **kwargs):
+            super().__init__(name=name, values=data)
+
+        def __repr__(self):
+            return repr(self)
+
+        def isnull(self):
+            return self.is_null()
 
 
-    def polars_read_csv(filepath, *args, **kwargs):
+    def _concat(objs, *args, **kwargs):
+        return pl.concat(objs, *args, **kwargs)
+
+    def _isnull(obj):
+        return obj.isnull()
+
+    def _isna(obj):
+        return obj.isnull()
+
+    def _merge(left, right, *args, **kwargs):
+        return left.join(right, *args, **kwargs)
+
+    def _read_csv(filepath, *args, **kwargs):
         return PolarsDataFrame(pl.read_csv(filepath))
+
 
     _DataFrame = PolarsDataFrame
     _Series = PolarsSeries
 
-    _concat = pd.concat
-    _isnull = pd.isnull
-    _isna = pd.isna
-    _merge = pd.merge
-    _read_csv = polars_read_csv
+    concat = _concat
+    isnull = _isnull
+    isna = _isna
+    merge = _merge
+    read_csv = _read_csv
 
 else:
     raise ValueError("Invalid value for BACKEND_DF environment variable. Use 'pd' or 'pl'.")
