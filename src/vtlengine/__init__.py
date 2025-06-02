d83b406bc40188c5b708e4c6b141e7e06e8beabe
javier.hernandez <73304358+javihern98@users.noreply.github.com>
2025-06-02 11:55:02 +0200

    Added __version__ on __init__

--- src/vtlengine/__init__.py
+++ src/vtlengine/__init__.py
@@ -1,3 +1,5 @@
 from vtlengine.API import generate_sdmx, prettify, run, run_sdmx, semantic_analysis
 
 __all__ = ["semantic_analysis", "run", "generate_sdmx", "run_sdmx", "prettify"]
+
+__version__ = "1.1.1"
