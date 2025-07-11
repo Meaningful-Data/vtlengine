51799edb040f618d829b24d09d5b22263b7cc0b1
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-07-11 12:43:05 +0200

    Minor fix.

--- src/vtlengine/Duckdb/duckdb_utils.py
+++ src/vtlengine/Duckdb/duckdb_utils.py
@@ -23,15 +23,14 @@ def duckdb_concat(left: DuckDBPyRelation, right: DuckDBPyRelation) -> DuckDBPyRe
     cols = set(left.columns) | set(right.columns)
     common_cols = set(left.columns).intersection(set(right.columns))
     cols_left = "*"
-    cols_right = "*"
     if common_cols:
-        if len(left.columns) > len(right.columns):
-            cols_left += f" EXCLUDE ({', '.join(common_cols)})"
-        else:
-            cols_right += f" EXCLUDE ({', '.join(common_cols)})"
+        cols_left += f" EXCLUDE ({', '.join(common_cols)})"
 
     left = left.project(f"{cols_left}, ROW_NUMBER() OVER () AS __row_id__").set_alias("base")
-    right = right.project(f"{cols_right}, ROW_NUMBER() OVER () AS __row_id__").set_alias("other")
+    right = right.project(f"*, ROW_NUMBER() OVER () AS __row_id__").set_alias("other")
+
+    print("Left columns:", left.columns)
+    print("Right columns:", right.columns)
     condition = "base.__row_id__ = other.__row_id__"
     return left.join(right, condition=condition, how="inner").project(", ".join(cols))
 
