5e4b813bb2fe46998ae08ba38fe95880d766e03f
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-07-22 12:34:07 +0200

    Removed invalid tests.

---
+++ src/vtlengine/duckdb/custom_functions/Null_checking.py
@@ -0,0 +1,5 @@
+from typing import Any
+
+
+def isnull_duck(x: Any) -> bool:
+    return x is None
--- src/vtlengine/duckdb/custom_functions/__init__.py
+++ src/vtlengine/duckdb/custom_functions/__init__.py
@@ -1,7 +1,9 @@
+from vtlengine.duckdb.custom_functions.Null_checking import isnull_duck as isnull
 from vtlengine.duckdb.custom_functions.Numeric import random_duck, round_duck, trunc_duck
 from vtlengine.duckdb.custom_functions.String import instr_duck, replace_duck, substr_duck, instr_check_param_value
 
 __all__ = [
+    "isnull",
     "random_duck",
     "trunc_duck",
     "round_duck",
