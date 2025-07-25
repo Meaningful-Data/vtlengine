f150a1bb2bca31d403e3505832c1771675495865
javierdc200 <javierdc200@gmail.com>
2025-07-25 10:27:28 +0200

    Implementation of the boolean operations

--- src/vtlengine/Operators/Boolean.py
+++ src/vtlengine/Operators/Boolean.py
@@ -1,11 +1,21 @@
-from typing import Any, Optional, Type
+from typing import Any, Type, Optional
 
-import pandas as pd
+from pandas.io.stata import excessive_string_length_error
 
 import vtlengine.Operators as Operator
 from vtlengine.AST.Grammar.tokens import AND, NOT, OR, XOR
 from vtlengine.DataTypes import Boolean
 
+def value_handled(value: Any) -> str:
+    if value is True or value == "True":
+        return "TRUE"
+    elif value is False or value == "False":
+        return "FALSE"
+    elif value is None or value == "None":
+        return "NULL"
+    elif isinstance(value, str):
+        return f'"{value}"'
+    return str(value)
 
 class Unary(Operator.Unary):
     type_to_check = Boolean
@@ -19,46 +29,122 @@ class Binary(Operator.Binary):
     @classmethod
     def apply_bin_op(cls: Type["Binary"], me_name: str, left: Any, right: Any) -> str:
         # TODO: Change this or remove this to use duckdb functions in Boolean
-        return Operator.apply_bin_op(cls, me_name, left, right)
+        if me_name is None:
+            return f"{cls.duck_op(left, right)}"
+        return f"{cls.duck_op(left, right)} AS {me_name}"
+
+    @classmethod
+    def duck_op(cls, left: str, right: str) -> str:
+        raise Exception("Method not allowed")
 
 
 class And(Binary):
     op = AND
 
-    @staticmethod
-    def py_op(x: Optional[bool], y: Optional[bool]) -> Optional[bool]:
-        if (x is None and y == False) or (x == False and y is None):
-            return False
-        elif x is None or y is None:
-            return None
-        return x and y
+    @classmethod
+    def apply_bin_op(cls, me_name: Optional[str], left: str, right: str) -> str:
+
+        if me_name is None:
+            return f"{cls.duck_op(left, right)}"
+        return f"{cls.duck_op(left, right)} AS \"{me_name}\""
+
+    @classmethod
+    def duck_op(cls, left: str, right: str) -> str:
+
+        query = f"""
+                CASE
+                    WHEN {left} IS FALSE OR {right} IS FALSE THEN FALSE
+                    WHEN {left} IS NULL OR {right} IS NULL THEN NULL
+                    ELSE TRUE
+                END
+                """
+        print(query)
+        return query
+
+    # @staticmethod
+    # def py_op(x: Optional[bool], y: Optional[bool]) -> Optional[bool]:
+    #     if (x is None and y == False) or (x == False and y is None):
+    #         return False
+    #     elif x is None or y is None:
+    #         return None
+    #     return x and y
 
 
 class Or(Binary):
     op = OR
 
-    @staticmethod
-    def py_op(x: Optional[bool], y: Optional[bool]) -> Optional[bool]:
-        if (x is None and y == True) or (x == True and y is None):
-            return True
-        elif x is None or y is None:
-            return None
-        return x or y
+    @classmethod
+    def apply_bin_op(cls, me_name: str, left: str, right: str) -> str:
+        if me_name is None:
+            return f"{cls.duck_op(left, right)}"
+        return f"{cls.duck_op(left, right)} AS \"{me_name}\""
+
+    @classmethod
+    def duck_op(cls, left: str, right: str) -> str:
+        return f"""
+            CASE
+                WHEN {left} IS TRUE OR {right} IS TRUE THEN TRUE
+                WHEN {left} IS NULL OR {right} IS NULL THEN NULL
+                ELSE FALSE
+            END
+            """
+
+    # @staticmethod
+    # def py_op(x: Optional[bool], y: Optional[bool]) -> Optional[bool]:
+    #     if (x is None and y == True) or (x == True and y is None):
+    #         return True
+    #     elif x is None or y is None:
+    #         return None
+    #     return x or y
 
 
 class Xor(Binary):
     op = XOR
 
     @classmethod
-    def py_op(cls, x: Optional[bool], y: Optional[bool]) -> Optional[bool]:
-        if pd.isnull(x) or pd.isnull(y):
-            return None
-        return (x and not y) or (not x and y)
+    def apply_bin_op(cls, me_name: str, left: str, right: str) -> str:
+        if me_name is None:
+            return f"{cls.duck_op(left, right)}"
+        return f"{cls.duck_op(left, right)} AS \"{me_name}\""
+
+    @classmethod
+    def duck_op(cls, left: str, right: str) -> str:
+        return f"""
+                CASE
+                    WHEN {left} IS NULL OR {right} IS NULL THEN NULL
+                    ELSE ({left} AND NOT {right}) OR (NOT {left} AND {right})
+                END
+                """
+
+    # @classmethod
+    # def py_op(cls, x: Optional[bool], y: Optional[bool]) -> Optional[bool]:
+    #     if pd.isnull(x) or pd.isnull(y):
+    #         return None
+    #     return (x and not y) or (not x and y)
 
 
 class Not(Unary):
     op = NOT
 
-    @staticmethod
-    def py_op(x: Optional[bool]) -> Optional[bool]:
-        return None if x is None else not x
+    @classmethod
+    def apply_unary_op(cls, measure_name: Optional[str], operand: str) -> str:
+        if measure_name is None:
+            return f"{cls.duck_op(operand)}"
+        else:
+            return f"{cls.duck_op(operand)} AS \"{measure_name}\""
+
+
+    @classmethod
+    def duck_op(cls, operand: str) -> str:
+        query = f"""
+                CASE
+                    WHEN {value_handled(operand)} IS NULL THEN NULL
+                    ELSE NOT {value_handled(operand)}
+                END
+                """
+        print(query)
+        return query
+
+    # @staticmethod
+    # def py_op(x: Optional[bool]) -> Optional[bool]:
+    #     return None if x is None else not x
--- src/vtlengine/Operators/__init__.py
+++ src/vtlengine/Operators/__init__.py
@@ -74,6 +74,8 @@ def handle_sql_scalar(value: Any) -> Any:
 def apply_unary_op(cls: Type["Unary"], me_name: str, value: Any) -> str:
     op = cls.op
     op_token = TO_SQL_TOKEN.get(op, op)
+    if hasattr(cls, "apply_unary_op"):
+        return cls.apply_unary_op(value,me_name)
     if isinstance(op_token, tuple):
         op_token, _ = op_token
     return f'{op_token}("{me_name}") AS "{value}"'
@@ -82,6 +84,10 @@ def apply_unary_op(cls: Type["Unary"], me_name: str, value: Any) -> str:
 def apply_unary_op_scalar(cls: Type["Unary"], value: Any) -> Any:
     op = cls.op
     op_token = TO_SQL_TOKEN.get(op, op)
+
+    if hasattr(cls, "apply_unary_op"):
+        return con.sql(f"SELECT{cls.apply_unary_op(None, value)}").fetchone()[0]
+
     if isinstance(op_token, tuple):
         op_token, _ = op_token
     result = con.sql(f"SELECT {op_token}({handle_sql_scalar(value)})").fetchone()[0]  # type: ignore[index]
@@ -89,6 +95,10 @@ def apply_unary_op_scalar(cls: Type["Unary"], value: Any) -> Any:
 
 
 def apply_bin_op(cls: Type["Binary"], me_name: str, left: Any, right: Any) -> str:
+
+    if hasattr(cls, "apply_bin_op"):
+        return cls.apply_bin_op(me_name, left, right)
+
     op = cls.op
     token_position = MIDDLE
     op_token = TO_SQL_TOKEN.get(op, op)
@@ -124,6 +134,10 @@ def apply_bin_op_scalar(cls: Type["Binary"], left: Any, right: Any) -> Any:
     query = (
         f"{op_token}({left}, {right})" if token_position == LEFT else f"({left} {op_token} {right})"
     )
+
+    if hasattr(cls, "apply_bin_op"):
+        query = cls.apply_bin_op(None, left, right)
+
     result = con.sql("SELECT " + query).fetchone()[0]  # type: ignore[index]
     return float(result) if isinstance(result, Decimal) else result
 
