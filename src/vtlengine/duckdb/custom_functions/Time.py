5ad7e0c247372ff612a04850c67a7f5b6b0b5d00
Alberto <155883871+albertohernandez1995@users.noreply.github.com>
2025-07-28 16:05:30 +0200

    Documentation on custom functions

--- src/vtlengine/duckdb/custom_functions/Time.py
+++ src/vtlengine/duckdb/custom_functions/Time.py
@@ -41,6 +41,16 @@ def year_duck(value: Optional[Union[date, str]]) -> Optional[int]:
 
 
 def month_duck(value: Optional[Union[date, str]]) -> Optional[int]:
+    """
+    Extracts the month from a date or time value.
+    Args:
+        value (Optional[Union[date, str]]): The date or time value,
+        which can be a date object or a string in ISO format.
+
+    Returns:
+        int | None: The extracted month as an integer, or None if the input is None or NaN.
+
+    """
     if value is None:
         return None
 
@@ -56,6 +66,15 @@ def month_duck(value: Optional[Union[date, str]]) -> Optional[int]:
 
 
 def day_of_month_duck(value: Optional[Union[date, str]]) -> Optional[int]:
+    """Extracts the day of the month from a date or time value.
+    Args:
+        value (Optional[Union[date, str]]): The date or time value,
+        which can be a date object or a string in ISO format.
+
+    Returns:
+        int | None: The extracted day of the month as an integer,
+        or None if the input is None or NaN.
+    """
     if value is None:
         return None
 
@@ -71,6 +90,15 @@ def day_of_month_duck(value: Optional[Union[date, str]]) -> Optional[int]:
 
 
 def day_of_year_duck(value: Optional[Union[date, str]]) -> Optional[int]:
+    """Extracts the day of the year from a date or time value.
+    Args:
+        value (Optional[Union[date, str]]): The date or time value,
+        which can be a date object or a string in ISO format.
+
+    Returns:
+        int | None: The extracted day of the year as an integer,
+        or None if the input is None or NaN.
+    """
     if value is None:
         return None
 
@@ -86,6 +114,16 @@ def day_of_year_duck(value: Optional[Union[date, str]]) -> Optional[int]:
 
 
 def day_to_year_duck(value: int) -> Optional[str]:
+    """
+    Converts a number of days into a string representation of years and remaining days.
+    Args:
+        value(int): The number of days to convert.
+
+    Returns:
+        str | None: A string in the format "P{years}Y{days}D" representing the duration,
+        or None if the input is None.
+
+    """
     if value is None:
         return None
     if isinstance(value, int):
@@ -100,6 +138,16 @@ def day_to_year_duck(value: int) -> Optional[str]:
 
 
 def day_to_month_duck(value: int) -> Optional[str]:
+    """
+    Converts a number of days into a string representation of months and remaining days.
+    Args:
+        value(int): The number of days to convert.
+
+    Returns:
+        str | None: A string in the format "P{months}M{days}D" representing the duration,
+        or None if the input is None.
+
+    """
     if value is None:
         return None
     if isinstance(value, int):
@@ -114,6 +162,14 @@ def day_to_month_duck(value: int) -> Optional[str]:
 
 
 def year_to_day_duck(value: str) -> Optional[int]:
+    """
+    Converts a duration string representing years into the equivalent number of days.
+    Args:
+        value(str): The duration string in the format "P{years}Y{days}D".
+
+    Returns:
+        The equivalent number of days as an integer, or None if the input is None.
+    """
     if value is None:
         return None
     if isinstance(value, str):
@@ -122,6 +178,15 @@ def year_to_day_duck(value: str) -> Optional[int]:
 
 
 def month_to_day_duck(value: str) -> Optional[int]:
+    """
+    Converts a duration string representing months into the equivalent number of days.
+    Args:
+        value(str): The duration string in the format "P{months}M{days}D".
+
+    Returns:
+        The equivalent number of days as an integer, or None if the input is None.
+
+    """
     if value is None:
         return None
     if isinstance(value, str):
@@ -130,6 +195,16 @@ def month_to_day_duck(value: str) -> Optional[int]:
 
 
 def date_diff_duck(x: Union[str, date], y: Union[str, date]) -> Optional[int]:
+    """
+    Calculates the absolute difference in days between two date.
+    Args:
+        x: Left date value, can be a string in ISO format or a date object.
+        y: Right date value, can be a string in ISO format or a date object.
+
+    Returns:
+        int | None: The absolute difference in days between the two dates, or None if either
+        date is None.
+    """
     if x is None:
         return None
     if y is None:
@@ -150,6 +225,18 @@ def date_diff_duck(x: Union[str, date], y: Union[str, date]) -> Optional[int]:
 
 
 def date_add_duck(value: Union[date, str], period: str, shift: int) -> date:
+    """
+    Adds a specified period and shift to a date or time value.
+    Args:
+        value(Union[date, str]): The date or time value to which the period will be added.
+        period(str): The period to add, which can be "D" (day), "W" (week), "M" (month),
+            "Q" (quarter), "S" (semester), or "A" (year).
+        shift(int): The number of periods to add. Positive values shift forward in time,
+            while negative values shift backward.
+
+    Returns:
+        date: The new date after adding the specified period and shift.
+    """
     if isinstance(value, str):
         tp_value = TimePeriodHandler(value)
         date_value = period_to_date(
@@ -176,6 +263,19 @@ def date_add_duck(value: Union[date, str], period: str, shift: int) -> date:
 def time_agg_duck(
     value: Optional[Union[str, date]], period_from: Optional[str], period_to: str, conf: str
 ) -> Optional[str]:
+    """
+    Aggregates a date or time value to a specified time period.
+    Args:
+        value (Optional[Union[str, date]]): The date or time value to aggregate.
+        period_from (Optional[str]): The time period to aggregate from, not used in this function.
+        period_to (str): The time period to aggregate to, such as "D", "W", "M", "Q", "S", or "A".
+        conf (str): Configuration parameter, where "first" indicates the start of the period
+            and any other value indicates the end of the period.
+
+    Returns:
+        str | None: The aggregated date or time value as a string in ISO format,
+        or None if the input value is None.
+    """
     if value is None:
         return None
     if isinstance(value, date):
