d73ca41f769ee7464028b86f32d0b9692ac1fa2f
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-07-29 12:00:25 +0200

    Fixed ruff errors.

--- src/vtlengine/duckdb/to_sql_token.py
+++ src/vtlengine/duckdb/to_sql_token.py
@@ -1,6 +1,6 @@
 from typing import Dict, Union
 
-from vtlengine.AST.Grammar.tokens import CHARSET_MATCH, LOG, MOD, NOT_IN, POWER, XOR, ISNULL
+from vtlengine.AST.Grammar.tokens import CHARSET_MATCH, ISNULL, LOG, MOD, NOT_IN, POWER, XOR
 
 # Could it be the operator sql token or a tuple of (sql token, token position)
 # default is taken as MIDDLE on Operator apply_operation method
