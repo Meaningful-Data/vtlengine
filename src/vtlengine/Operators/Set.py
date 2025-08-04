8a015c79ebff5ad46d6287aabcc2f05e03268ef1
javierdc200 <javierdc200@gmail.com>
2025-08-04 09:33:01 +0200

    Implementation of Sets operators

--- src/vtlengine/Operators/Set.py
+++ src/vtlengine/Operators/Set.py
@@ -1,11 +1,11 @@
 from typing import Any, Dict, List
 
-import pandas as pd
-
+from vtlengine.connection import con
 from vtlengine.DataTypes import binary_implicit_promotion
 from vtlengine.Exceptions import SemanticError
 from vtlengine.Model import Dataset
 from vtlengine.Operators import Operator
+from vtlengine.Utils.__Virtual_Assets import VirtualCounter
 
 
 class Set(Operator):
@@ -61,11 +61,34 @@ class Union(Set):
     @classmethod
     def evaluate(cls, operands: List[Dataset]) -> Dataset:
         result = cls.validate(operands)
-        all_datapoints = [ds.data for ds in operands]
-        result.data = pd.concat(all_datapoints, sort=True, ignore_index=True)
-        identifiers_names = result.get_identifiers_names()
-        result.data = result.data.drop_duplicates(subset=identifiers_names, keep="first")
-        result.data.reset_index(drop=True, inplace=True)
+        queries = []
+        id_names = operands[0].get_identifiers_names()
+        ids_str = ", ".join(id_names)
+        col_names = list(operands[0].data.columns)
+        cols_str = ", ".join(col_names)
+
+        for i in range(len(operands)):
+            name = VirtualCounter._new_temp_view_name()
+            con.register(name, operands[i].data)
+            queries.append(f"SELECT {cols_str}, {i} AS prioridad FROM {name}")
+
+        union_query = " UNION ALL ".join(queries)
+
+        final_query = f"""
+                    WITH unioned AS (
+                        {union_query}
+                    ),
+                    pole AS (
+                        SELECT *,
+                            ROW_NUMBER() OVER (PARTITION BY {ids_str} ORDER BY prioridad ASC) AS rn
+                        FROM unioned
+                    )
+                    SELECT {cols_str}
+                    FROM pole
+                    WHERE rn = 1
+                """
+
+        result.data = con.query(final_query)
         return result
 
 
@@ -73,25 +96,20 @@ class Intersection(Set):
     @classmethod
     def evaluate(cls, operands: List[Dataset]) -> Dataset:
         result = cls.validate(operands)
-        all_datapoints = [ds.data for ds in operands]
-        for data in all_datapoints:
-            if result.data is None:
-                result.data = data
-            else:
-                if data is None:
-                    result.data = pd.DataFrame(columns=result.get_identifiers_names())
-                    break
-                result.data = result.data.merge(
-                    data, how="inner", on=result.get_identifiers_names()
-                )
 
-                not_identifiers = result.get_measures_names() + result.get_attributes_names()
+        col_names = list(operands[0].data.columns)
+        cols_str = ", ".join(col_names)
 
-                for col in not_identifiers:
-                    result.data[col] = result.data[col + "_x"]
-                result.data = result.data[result.get_identifiers_names() + not_identifiers]
-        if result.data is not None:
-            result.data.reset_index(drop=True, inplace=True)
+        list_names = []
+        for ds in operands:
+            temp_name = VirtualCounter._new_temp_view_name()
+            con.register(temp_name, ds.data)
+            list_names.append(temp_name)
+
+        sub_query = [f"SELECT {cols_str} FROM {name}" for name in list_names]
+        query = " INTERSECT ".join(sub_query)
+
+        result.data = con.query(query)
         return result
 
 
@@ -99,73 +117,73 @@ class Symdiff(Set):
     @classmethod
     def evaluate(cls, operands: List[Dataset]) -> Dataset:
         result = cls.validate(operands)
-        all_datapoints = [ds.data for ds in operands]
-        for data in all_datapoints:
-            if data is None:
-                data = pd.DataFrame(columns=result.get_identifiers_names())
-            if result.data is None:
-                result.data = data
-            else:
-                result.data = result.data.merge(
-                    data,
-                    how="outer",
-                    on=result.get_identifiers_names(),
-                    suffixes=("_x", "_y"),
-                )
 
-                for measure in result.get_measures_names():
-                    result.data["_merge"] = result.data.apply(
-                        lambda row: (
-                            "left_only"
-                            if pd.isnull(row[f"{measure}_y"])
-                            else ("right_only" if pd.isnull(row[f"{measure}_x"]) else "both")
-                        ),
-                        axis=1,
-                    )
+        id_names = operands[0].get_identifiers_names()
+        ids_str = ", ".join(id_names)
 
-                not_identifiers = result.get_measures_names() + result.get_attributes_names()
-                for col in not_identifiers:
-                    result.data[col] = result.data.apply(
-                        lambda x, c=col: (
-                            x[c + "_x"]
-                            if x["_merge"] == "left_only"
-                            else (x[c + "_y"] if x["_merge"] == "right_only" else None)
-                        ),
-                        axis=1,
+        ds1 = operands[0]
+        ds2 = operands[1]
+        name1 = VirtualCounter._new_temp_view_name()
+        name2 = VirtualCounter._new_temp_view_name()
+
+        con.register(name1, ds1.data)
+        con.register(name2, ds2.data)
+
+        col_names = list(operands[0].data.columns)
+        cols_str = ", ".join(col_names)
+
+        diff1 = f"""
+                    SELECT {name1}.*, 0 AS prioridad
+                    FROM {name1}
+                    LEFT JOIN {name2}
+                    ON {" AND ".join([f"{name1}.{col} = {name2}.{col}" for col in id_names])}
+                    WHERE {name2}.{id_names[0]} IS NULL
+                """
+
+        diff2 = f"""
+                    SELECT {name2}.*, 1 AS prioridad
+                    FROM {name2}
+                    LEFT JOIN {name1}
+                    ON {" AND ".join([f"{name2}.{col} = {name1}.{col}" for col in id_names])}
+                    WHERE {name1}.{id_names[0]} IS NULL
+                """
+
+        final_query = f"""
+                    WITH symdiff AS (
+                        {diff1}
+                        UNION ALL
+                        {diff2}
+                    ),
+                    pole AS (
+                        SELECT *,
+                            ROW_NUMBER() OVER (PARTITION BY {ids_str} ORDER BY prioridad ASC) AS rn
+                        FROM symdiff
                     )
-                result.data = result.data[result.get_identifiers_names() + not_identifiers].dropna()
-        if result.data is not None:
-            result.data = result.data.reset_index(drop=True)
+                    SELECT {cols_str}
+                    FROM pole
+                    WHERE rn = 1
+                """
+
+        result.data = con.query(final_query)
         return result
 
 
 class Setdiff(Set):
-    @staticmethod
-    def has_null(row: Any) -> bool:
-        return row.isnull().any()
-
     @classmethod
     def evaluate(cls, operands: List[Dataset]) -> Dataset:
         result = cls.validate(operands)
-        all_datapoints = [ds.data for ds in operands]
-        for data in all_datapoints:
-            if result.data is None:
-                result.data = data
-            else:
-                if data is None:
-                    data = pd.DataFrame(columns=result.get_identifiers_names())
-                result.data = result.data.merge(data, how="left", on=result.get_identifiers_names())
-                if len(result.data) > 0:
-                    result.data = result.data[result.data.apply(cls.has_null, axis=1)]
 
-                not_identifiers = result.get_measures_names() + result.get_attributes_names()
-                for col in not_identifiers:
-                    if col + "_x" in result.data:
-                        result.data[col] = result.data[col + "_x"]
-                        del result.data[col + "_x"]
-                    if col + "_y" in result.data:
-                        del result.data[col + "_y"]
-                result.data = result.data[result.get_identifiers_names() + not_identifiers]
-        if result.data is not None:
-            result.data.reset_index(drop=True, inplace=True)
+        ds1, ds2 = operands
+        name1 = VirtualCounter._new_temp_view_name()
+        name2 = VirtualCounter._new_temp_view_name()
+
+        con.register(name1, ds1.data)
+        con.register(name2, ds2.data)
+
+        query = f"""
+            SELECT * FROM {name1}
+            EXCEPT
+            SELECT * FROM {name2}
+        """
+        result.data = con.query(query)
         return result
