8378f44a16471f311f0a7f502ce8b5ba67653790
Javier <javier.hernandez@meaningfuldata.eu>
2025-08-05 09:23:25 +0200

    Fixed mypy errors

--- src/vtlengine/Operators/Aggregation.py
+++ src/vtlengine/Operators/Aggregation.py
@@ -103,12 +103,12 @@ class Aggregation(Operator.Unary):
             elif measure.data_type == Duration:
                 if mode == "input":
                     data[measure.name] = data[measure.name].map(
-                        lambda x: PERIOD_IND_MAPPING[x],  # type: ignore[index]
+                        lambda x: PERIOD_IND_MAPPING[x],
                         na_action="ignore",
                     )
                 else:
                     data[measure.name] = data[measure.name].map(
-                        lambda x: PERIOD_IND_MAPPING_REVERSE[x],  # type: ignore[index]
+                        lambda x: PERIOD_IND_MAPPING_REVERSE[x],
                         na_action="ignore",
                     )
             elif measure.data_type == Boolean:
