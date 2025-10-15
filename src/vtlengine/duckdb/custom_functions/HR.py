085d93bf80ffb8528fd9a412a6604181a02bf716
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-10-15 17:30:07 +0200

    Fixed handle mode return value casted as str.

--- src/vtlengine/duckdb/custom_functions/HR.py
+++ src/vtlengine/duckdb/custom_functions/HR.py
@@ -8,9 +8,7 @@ def imbalance_func(x: Any, y: Any) -> Optional[float]:
     return None if x is None or y is None else x - y
 
 
-def handle_mode(x: Any, hr_mode: str) -> str:
-    if x is not None and x == NINF:
-        return NINF
+def handle_mode(x: Any, hr_mode: str) -> float:
     if hr_mode == "non_null" and x is None or hr_mode == "non_zero" and x == 0:
         return NINF
     return x
