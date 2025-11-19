23b53c326be6d3bcf2ed7beb1e849c2c42c258dc
Mateo <mateo.delorenzo@meaningfuldata.eu>
2025-11-19 14:00:15 +0100

    Added related test

--- tests/AST/test_AST.py
+++ tests/AST/test_AST.py
@@ -40,7 +40,9 @@ from vtlengine.Interpreter import InterpreterAnalyzer
 base_path = Path(__file__).parent
 filepath = base_path / "data" / "encode"
 
-param = ["DS_r := DS_1 + DS_2;"]
+param = ["DS_r := DS_1 + DS_2;",
+         """DS_r := DS_1 + DS_2;
+         //comment"""]
 
 
 param_ast = [
