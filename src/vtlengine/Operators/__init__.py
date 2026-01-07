96a0299c00d24455c27521bd1257f14b5b91cbf2
Mateo <mateo.delorenzo@meaningfuldata.eu>
2026-01-07 13:49:40 +0100

    Fixed Series level operation reindexing new series.

--- src/vtlengine/Operators/__init__.py
+++ src/vtlengine/Operators/__init__.py
@@ -3,10 +3,6 @@ import re
 from copy import copy
 from typing import Any, Optional, Union
 
-# if os.environ.get("SPARK", False):
-#     import pyspark.pandas as pd
-# else:
-#     import pandas as pd
 import pandas as pd
 
 from vtlengine.AST.Grammar.tokens import (
@@ -57,7 +53,6 @@ class Operator:
 
     op: Any = None
     py_op: Any = None
-    spark_op: Any = None
     type_to_check: Any = None
     return_type: Any = None
 
@@ -234,25 +229,18 @@ class Binary(Operator):
         return cls.py_op(x, y)
 
     @classmethod
-    def apply_operation_two_series(cls, left_series: Any, right_series: Any) -> Any:
-        if os.getenv("SPARK", False):
-            if cls.spark_op is None:
-                cls.spark_op = cls.py_op
-
-            nulls = left_series.isnull() | right_series.isnull()
-            result = cls.spark_op(left_series, right_series)
-            result.loc[nulls] = None
-            return result
+    def apply_operation_two_series(cls, left_series: pd.Series, right_series: pd.Series) -> pd.Series:
         result = list(map(cls.op_func, left_series.values, right_series.values))
-        return pd.Series(result, index=list(range(len(result))), dtype=object)
+        index = left_series.index if len(left_series) <= len(right_series) else right_series.index
+        return pd.Series(result, index=index, dtype=object)
 
     @classmethod
     def apply_operation_series_scalar(
         cls,
-        series: Any,
+        series: pd.Series,
         scalar: Scalar,
         series_left: bool,
-    ) -> Any:
+    ) -> pd.Series:
         if scalar is None:
             return pd.Series(None, index=series.index)
         if series_left:
