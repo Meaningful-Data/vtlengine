12e72f048c804dd677f5bc36ff6eb97f1d29ab35
Mateo de Lorenzo Argelés <160473799+mla2001@users.noreply.github.com>
2026-03-10 13:52:53 +0100

    Fix #582: Fixed time_agg grammar with single string constant in group_all and windowing (#584)
    * Grammar aligned with the official VTL 2.1
    
    * Regenerated Lexer, Parser and VTLVisitor
    
    * Fixed related tests
    
    * Fixed mypy errors

--- tests/TimePeriod/test_timeperiod.py
+++ tests/TimePeriod/test_timeperiod.py
@@ -24,9 +24,9 @@ ds_param = [
         "GL_416",
         'test2_1 := BE2_DF_NICP[filter FREQ = "M" and TIME_PERIOD = cast("2020-01", time_period)];',
     ),
-    ("GL_417_1", 'test := avg (BE2_DF_NICP group all time_agg ("Q", "M", TIME_PERIOD));'),
-    ("GL_417_2", 'test := avg (BE2_DF_NICP group all time_agg ("A", "M", TIME_PERIOD));'),
-    ("GL_417_4", 'test := avg (BE2_DF_NICP group all time_agg ("A", "Q", TIME_PERIOD));'),
+    ("GL_417_1", 'test := avg (BE2_DF_NICP group all time_agg ("Q"));'),
+    ("GL_417_2", 'test := avg (BE2_DF_NICP group all time_agg ("A"));'),
+    ("GL_417_4", 'test := avg (BE2_DF_NICP group all time_agg ("A"));'),
     (
         "GL_418",
         'test2_1 := BE2_DF_NICP[sub DERIVATION = "INDICES"][filter FREQ = "M"][keep OBS_VALUE]; \
@@ -39,7 +39,7 @@ ds_param = [
         "GL_421_1",
         'test2_1 := BE2_DF_NICP[calc FREQ_2 := TIME_PERIOD in {cast("2020-01", time_period), cast("2021-01", time_period)}];',
     ),
-    # ("GL_421_2", 'test := avg (BE2_DF_NICP group all time_agg ("A", "M", TIME_PERIOD));'),
+    # ("GL_421_2", 'test := avg (BE2_DF_NICP group all time_agg ("A"));'),
     ("GL_440_1", "DS_r := DS_1;"),
     ("GL_462_1", "added := demo_data_structure;"),
     ("GL_462_2", "added := demo_data_structure; DS_r := added+ ds_2;"),
