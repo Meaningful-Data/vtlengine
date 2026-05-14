0e85b145b6255b281b6bbe7a8f1d43f5545be6b2
Alberto <alberto.hernandez@meaningfuldata.eu>
2026-05-14 12:36:24 +0200

    Deleted unused function

--- src/vtlengine/files/output/__init__.py
+++ src/vtlengine/files/output/__init__.py
@@ -33,24 +33,3 @@ def format_date_iso8601(operand: Union[Dataset, Scalar]) -> None:
                 .map(_space_to_t, na_action="ignore")
                 .astype("string[pyarrow]")
             )
-
-
-def save_datapoints(
-    time_period_representation: Optional[TimePeriodRepresentation],
-    dataset: Dataset,
-    output_path: Union[str, Path],
-) -> None:
-    if dataset.data is None:
-        dataset.data = pd.DataFrame()
-    format_date_iso8601(dataset)
-    if time_period_representation is not None:
-        format_time_period_external_representation(dataset, time_period_representation)
-
-    # Get float format based on environment configuration
-    float_format = get_float_format()
-
-    if isinstance(output_path, str):
-        output_path = Path(output_path)
-
-    output_file = output_path / f"{dataset.name}.csv"
-    dataset.data.to_csv(output_file, index=False, float_format=float_format)
