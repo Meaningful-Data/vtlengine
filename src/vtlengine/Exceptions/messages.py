f7979261781e1a727fbd056fb01853016597b729
Alberto <alberto.hernandez@meaningfuldata.eu>
2026-05-20 12:44:50 +0200

    Added new error messsages

--- src/vtlengine/Exceptions/messages.py
+++ src/vtlengine/Exceptions/messages.py
@@ -781,6 +781,18 @@ centralised_messages = {
         "description": "Occurs when a Dataset is incorrectly used as a parameter in a "
         "string operation.",
     },
+    "1-1-18-11": {
+        "message": "At op {op}: Invalid distance method {method}. Expected one of "
+        "{expected_methods}.",
+        "description": "Raised when the first argument of string_distance is not one of the "
+        "supported methods (levenshtein, damerau_levenshtein, hamming, jaro_winkler).",
+    },
+    "1-1-18-12": {
+        "message": "At op {op}: Hamming distance requires both strings to have the same length, "
+        "got lengths {len1} and {len2}.",
+        "description": "Raised when the hamming method of string_distance is invoked on two "
+        "strings of unequal length.",
+    },
     # Time operators
     "1-1-19-1": {
         "message": "At op {op}: {op} must have a {data_type} type on {comp}.",
