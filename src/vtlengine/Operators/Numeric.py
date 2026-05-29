27530596aec582fdaa1220e4fb1e544758444710
Mateo <mateo.delorenzo@meaningfuldata.eu>
2026-05-29 16:27:51 +0200

    Fixed ruff errors

--- src/vtlengine/Operators/Numeric.py
+++ src/vtlengine/Operators/Numeric.py
@@ -375,9 +375,7 @@ class Parameterized(Unary):
                     comp_name=measure_name,
                     dataset_name=operand.name,
                 ) from None
-        result.data = result.data[
-            operand.get_identifiers_names() + operand.get_measures_names()
-        ]
+        result.data = result.data[operand.get_identifiers_names() + operand.get_measures_names()]
         cls.modify_measure_column(result)
         return result
 
